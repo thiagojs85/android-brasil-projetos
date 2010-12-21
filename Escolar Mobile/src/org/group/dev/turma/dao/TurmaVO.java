a22cb4b718d46c4bb52e8c64a64a6e840090f396
rofatto <rofatto@38958056-2125-7567-1d32-3ddec9877234>
2010-12-21 16:53:06 +0000

    Criação do Adaptador para comunicação com o banco de dados (DbAdapter) e DAO para os dados da Turma.

---
+++ Escolar Mobile/src/org/group/dev/turma/dao/TurmaVO.java
@@ -0,0 +1,26 @@
+package org.group.dev.turma.dao;
+
+public class TurmaVO {
+	private String nome;
+	private String descricao;
+	private int id;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
