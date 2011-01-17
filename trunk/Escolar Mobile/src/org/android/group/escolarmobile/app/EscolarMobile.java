5b713eb62c5b7cfb33ee7141348997fcda03b9d7
rofatto@gmail.com <rofatto@gmail.com@38958056-2125-7567-1d32-3ddec9877234>
2011-01-17 21:16:21 +0000

    Atualização do padrão de pacotes.

---
+++ trunk/Escolar Mobile/src/org/android/group/escolarmobile/app/EscolarMobile.java
@@ -0,0 +1,57 @@
+package org.android.group.escolarmobile.app;
+
+
+import org.group.dev.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+/**
+ * Classe inicial do app.
+ * 
+ * @author Neto
+ *
+ */
+public class EscolarMobile extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button btVisualizar = (Button) findViewById(R.id.visualizar);
+        
+        Spinner turmas = (Spinner) findViewById(R.id.sp_turmas);
+        ArrayAdapter<CharSequence> adapterTurmas = ArrayAdapter.createFromResource(
+                this, R.array.turmas, android.R.layout.simple_spinner_item);
+        adapterTurmas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        turmas.setAdapter(adapterTurmas);
+        turmas.setSelection(9);
+        
+        Spinner materias = (Spinner) findViewById(R.id.sp_materias);
+        ArrayAdapter<CharSequence> adapterMaterias = ArrayAdapter.createFromResource(
+                this, R.array.materias, android.R.layout.simple_spinner_item);
+        adapterMaterias.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        materias.setAdapter(adapterMaterias);
+        materias.setSelection(6);
+        
+        AutoCompleteTextView alunos = (AutoCompleteTextView) findViewById(R.id.AC_alunos);
+        String[] arrayAlunos = getResources().getStringArray(R.array.alunos);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, R.layout.list_item, arrayAlunos);
+        alunos.setAdapter(adapter3);
+        
+        
+        btVisualizar.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Implementar função de busca e visualização.
+				Toast.makeText(EscolarMobile.this, "Botão Visualizar Pressionado!", Toast.LENGTH_LONG).show();
+			}
+		});
+    }
+}
---
+++ trunk/Escolar Mobile/src/org/android/group/escolarmobile/turma/TurmaVO.java
@@ -0,0 +1,32 @@
+package org.android.group.escolarmobile.turma;
+
+/**
+ * Esta classe é apenas um acesso para os dados de uma turma.
+ * 
+ * @author Otavio
+ *
+ */
+public class TurmaVO {
+	private String nome;
+	private String descricao;
+	private int id;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
