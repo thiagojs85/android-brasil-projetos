3f2ac552c616360a2b03a8dff09aa483a7d2250b
juliocbcotta@gmail.com <juliocbcotta@gmail.com@38958056-2125-7567-1d32-3ddec9877234>
2011-01-18 04:00:41 +0000

    Adicionados comentarios de TODO

--- trunk/Escolar Mobile/src/org/android/group/escolarmobile/app/AddClass.java
+++ trunk/Escolar Mobile/src/org/android/group/escolarmobile/app/AddClass.java
@@ -2,6 +2,7 @@ package org.android.group.escolarmobile.app;
 
 import org.group.dev.R;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,11 +21,15 @@ public class AddClass extends BaseListWindow {
     
 	@Override
 	public void onClick(View v) {
+		//TODO: Chamar CLASSE_DO_NETO abaixo
+    	//Intent i = new Intent(this, CLASSE_DO_NETO.class);
+    	//startActivityForResult(i, 0);
 		Toast.makeText(AddClass.this, "Botão Adicionar Pressionado!", Toast.LENGTH_LONG).show();
 	}
 	
 	@Override
 	public void setStringItens() {
+		//TODO: Preencher com o nome de cada turma
 		//this.itens = new String[]{"linha um","linha dois","linha tres"};
 		this.itens = new String[]{};
 	}
