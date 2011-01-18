c31845e2efea2be2d53793f38e53840b760b24b4
juliocbcotta <juliocbcotta@gmail.com>
2011-01-18 05:46:50 +0000

    Adicionado menu de contexto menu para edição e remoção. agora existem metodos na classe AddClass.java dar suporte a essas ações.

--- Escolar Mobile/src/org/android/group/escolarmobile/app/AddClass.java
+++ Escolar Mobile/src/org/android/group/escolarmobile/app/AddClass.java
@@ -1,10 +1,12 @@
 package org.android.group.escolarmobile.app;
 
+import org.android.group.escolarmobile.conn.DbAdapter;
 import org.group.dev.R;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -28,9 +30,32 @@ public class AddClass extends BaseListWindow {
 	}
 	
 	@Override
-	public void setStringItens() {
+	public void fillItens() {
 		//TODO: Preencher com o nome de cada turma
-		//this.itens = new String[]{"linha um","linha dois","linha tres"};
-		this.itens = new String[]{};
+		this.itens = new String[]{"linha 0","linha 1","linha 2","linha 3","linha 4","linha 5","linha 6","linha 7","linha 8"};
+		//setItens(new String[]{});
 	}
+	@Override
+	public void setActionOnEditItem(MenuItem item){
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		
+/*
+		//TODO: Chamar CLASSE_DO_NETO abaixo
+    	Intent i = new Intent(this, CLASSE_DO_NETO.class);
+		Bundle b = new Bundle();
+		//TODO: itens[(int)info.id] retorna o nome da turma que pode ser passado para a CLASSE_DO_NETO quando for editar uma turma
+		b.putInt(DbAdapter.COLUMN_NOME, itens[(int)info.id]);
+		i.putExtras(b);
+    	startActivityForResult(i, EDIT_ID);
+*/
+
+		Toast.makeText(AddClass.this, "Botão Editar Pressionado! "+itens[(int)info.id], Toast.LENGTH_LONG).show();
+	}
+	
+	@Override
+	public void setActionOnDeleteItem(MenuItem item){
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		Toast.makeText(AddClass.this, "Botão delete Pressionado!", Toast.LENGTH_LONG).show();
+	}
+	
 }
--- Escolar Mobile/src/org/android/group/escolarmobile/app/BaseListInterface.java
+++ Escolar Mobile/src/org/android/group/escolarmobile/app/BaseListInterface.java
@@ -2,5 +2,5 @@ package org.android.group.escolarmobile.app;
 
 public interface BaseListInterface  {
 
-	public void setStringItens();
+	public void fillItens();
 }
