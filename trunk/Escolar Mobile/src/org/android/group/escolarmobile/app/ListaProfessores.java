30967eb9660c8a7a513c2f883b9ec777300e1b52
desilio@gmail.com <desilio@gmail.com@38958056-2125-7567-1d32-3ddec9877234>
2011-01-24 12:05:25 +0000

    Foi criado a opção professores no menu da tela principal da aplicação e criada a classe ListaProfessores.

---
+++ trunk/Escolar Mobile/src/org/android/group/escolarmobile/app/ListaProfessores.java
@@ -0,0 +1,102 @@
+package org.android.group.escolarmobile.app;
+
+import org.android.group.escolarmobile.conn.DbAdapter;
+import org.group.dev.R;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+
+/**
+* Classe responsavel por listar os professores cadastrados e adicionar mais se necessário
+*
+* @author Neto
+*/
+
+
+public class ListaProfessores extends BasicListWindow {
+	
+	private static final int DIALOG_DELETAR = 0;
+	private long idDelete;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button ibt = (Button)findViewById(R.id.add);
+        ibt.setText(R.string.adicionar_professor);//sobrescreve a string original do xml
+    }
+    
+    
+    @Override
+	protected boolean isMultiItensSelectable() {
+		// Caso queira que os itens da lista sejam selecionaveis, fazer retornar true
+		return false;
+	}
+    
+    
+	@Override
+	public void onClick(View v) {
+		
+		//TODO: Inciar a classe de edição de professores
+    	//Intent i = new Intent(this, ClasseDoDiego.class);
+    	//startActivityForResult(i, ADD_ID);
+	}
+	
+	@Override
+	public void fillItens() {
+		this.itens = mDbAdapter.consultarTodos(DbAdapter.TABLE_PROFESSOR, DbAdapter.COLUMN_NOME).toArray(new String[]{});
+	}
+	
+	@Override
+	public void setActionOnEditItem(MenuItem item){
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+				
+		/*Intent i = new Intent(this, ClasseDoDiego.class);
+
+    	 Repassa o id da linha selecionada.
+		Bundle b = new Bundle();
+		b.putLong(DbAdapter.COLUMN_ID, ((Long) getListAdapter().getItem(info.position)).longValue());
+		i.putExtras(b);
+    	startActivityForResult(i, EDIT_ID);*/
+	}
+	
+	@Override
+	public void setActionOnDeleteItem(MenuItem item){
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		idDelete = ((Long) getListAdapter().getItem(info.position)).longValue();
+		showDialog(DIALOG_DELETAR);
+	}
+	
+	/**
+	 * Fun��o que cria os di�logos utilizados nesta activity.
+	 * 
+	 * @param id identifica��o do di�logo que deve ser criado.
+	 */
+	protected Dialog onCreateDialog(int id) {
+		switch(id) {
+			case DIALOG_DELETAR:
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setMessage(R.string.dialog_delete).setCancelable(false);
+				builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						mDbAdapter.removerTurma(idDelete);
+					}
+				});
+				builder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+				return builder.create();
+			default:
+				return null;
+		}
+	}
+
+}
