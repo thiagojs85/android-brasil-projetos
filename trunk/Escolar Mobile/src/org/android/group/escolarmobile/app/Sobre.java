989c7bf3c0d4a29a2704e1bbff7cc13ee7b20092
felipetgoliveira@gmail.com <felipetgoliveira@gmail.com@38958056-2125-7567-1d32-3ddec9877234>
2011-02-24 02:23:24 +0000

    Sobre: Primeira versão

---
+++ trunk/Escolar Mobile/src/org/android/group/escolarmobile/app/Sobre.java
@@ -0,0 +1,14 @@
+package org.android.group.escolarmobile.app;
+
+import org.group.dev.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sobre extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sobre);
+	}
+}
