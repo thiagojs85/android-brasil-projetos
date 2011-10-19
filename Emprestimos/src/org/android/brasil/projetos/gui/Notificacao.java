f0f0c439b6bea6aed2c16689779bb84a2bf7c0aa
gustavodacosta <gustavodacosta@gmail.com>
2011-10-19 01:15:19 +0000

    notificatiob

---
+++ Emprestimos/src/org/android/brasil/projetos/gui/Notificacao.java
@@ -0,0 +1,43 @@
+package org.android.brasil.projetos.gui;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Exemplo de Activity que cria uma notifica��o
+ * 
+ * @author ricardo
+ * 
+ */
+public class Notificacao extends Activity {
+
+	@Override
+	protected void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+
+		String tickerText = "Empréstimo";
+		CharSequence titulo = "Empréstimo";
+		CharSequence mensagem = "Devolução/Recebimento";
+
+		criarNotificacao(this, tickerText, titulo, mensagem, EditarEmprestimo.class);
+	}
+
+	protected void criarNotificacao(Context context, CharSequence mensagemBarraStatus, CharSequence titulo,CharSequence mensagem, Class<?> activity) {
+
+		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+		Notification n = new Notification(R.drawable.icon, mensagemBarraStatus, System.currentTimeMillis());
+
+		PendingIntent p = PendingIntent.getActivity(this, 0, new Intent(this, activity), 0);
+
+		n.setLatestEventInfo(this, titulo, mensagem, p);
+		n.vibrate = new long[] { 100, 250, 100, 500 };
+
+		nm.notify(R.string.app_name, n);
+	}
+}
