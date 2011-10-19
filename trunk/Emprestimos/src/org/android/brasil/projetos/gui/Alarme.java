7b7bb1dd9c0da3833172a0fa9a74741fa75a0cb7
gustavodacosta@gmail.com <gustavodacosta@gmail.com@38958056-2125-7567-1d32-3ddec9877234>
2011-10-19 01:15:19 +0000

    notificatiob

--- trunk/Emprestimos/src/org/android/brasil/projetos/gui/Alarme.java
+++ trunk/Emprestimos/src/org/android/brasil/projetos/gui/Alarme.java
@@ -5,7 +5,6 @@ import org.android.brasil.projetos.dao.EmprestimoDAO;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 public class Alarme extends BroadcastReceiver {
 	private long mRowid;
@@ -13,12 +12,15 @@ public class Alarme extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		mRowid = intent.getLongExtra(EmprestimoDAO.COLUNA_ID_EMPRESTIMO, 0);
 		if(mRowid > 0){
-			Intent i = new Intent(context, EditarEmprestimo.class);
+			Intent i = new Intent(context, Notificacao.class);
 			i.putExtras(intent.getExtras());
 			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			Toast.makeText(context, "Alarme ativado!", Toast.LENGTH_LONG).show();
+			//Toast.makeText(context, "Alarme ativado!", Toast.LENGTH_LONG).show();
 			context.startActivity(i);
+			
 		}
 		
 	}
+	
+	
 }
